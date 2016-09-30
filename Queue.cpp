ccebf9779a7868dcf9af87f2cc3c4e2e1df0936a
Timothy Haskins <tijohask@iupui.edu>
2016-09-29 21:22:57 -0400

    Added explanatory comments, eliminated redundant code

--- Queue.cpp
+++ Queue.cpp
@@ -27,13 +27,10 @@ array(50), cur_size_(0), max_size_(50), is_empty_(true)
 //
 template <typename T>
 Queue <T>::Queue (const Queue & queue): 
-array(NULL), cur_size_(queue.size()), max_size_(queue.max_size()), is_empty_(queue.is_empty())
+array(queue.max_size()), cur_size_(queue.size()), max_size_(queue.max_size()), is_empty_(queue.is_empty())
 {
-	array = Array <T> (queue.max_size());
-	cur_size_ = queue.size();
-	max_size_ = queue.max_size();
 	for(int i = 0; i < cur_size_; i++)
-	{
+	{//iterate through the array, copying the queue
 		array[i] = queue.get(i);
 	}
 }
@@ -53,13 +50,13 @@ Queue <T>::~Queue (void)
 template <typename T>
 const Queue <T> & Queue <T>::operator = (const Queue & rhs)
 {
-	if(this != &rhs)
+	if(this != &rhs)//check pointers
 	{
-		array = Array <T> (rhs.max_size());
-		cur_size_ = rhs.size();
+		array = Array <T> (rhs.max_size());//declare a new array to match the rhs
+		cur_size_ = rhs.size();//assign the sizes
 		max_size_ = rhs.max_size();
 		for(int i = 0; i < cur_size_; i++)
-		{
+		{//iterate through, copying the queue
 			array[i] = rhs.get(i);
 		}
 	}
@@ -68,6 +65,13 @@ const Queue <T> & Queue <T>::operator = (const Queue & rhs)
 //
 // get
 //
+/**
+ * Genuine question: is there a better way to do this?
+ * I put this get method in here to be able to copy the 
+ * Queue from one to another. Array is private, after all,
+ * so there is no direct means of getting to it, but...
+ * I dunno. I feel like I missed something.
+ */
 template <typename T>
 T Queue <T>::get (size_t index) const
 {
@@ -81,13 +85,13 @@ template <typename T>
 void Queue <T>::enqueue (T element)
 {
 	if(cur_size_ == max_size_)
-	{
+	{//resize the array as needed
 		array.resize(max_size_ * 2);
 	}
-	array.set(cur_size_, element);
+	array.set(cur_size_, element);//put the element at the end of the queue
 	cur_size_ = cur_size_ + 1;
 	if(is_empty_)
-	{
+	{//make sure is_empty_ is false
 		is_empty_ = false;
 	}
 }
@@ -98,17 +102,17 @@ void Queue <T>::enqueue (T element)
 template <typename T>
 T Queue <T>::dequeue (void)
 {
-	T hold = array[0];
+	T hold = array[0];//store the value to be returned
 	
 	for(int i = 0; i < cur_size_ - 1; i++)
-	{
+	{//move everything forward one unit
 		array[i] = array[i + 1];
 	}
 	
-	cur_size_ -= 1;
+	cur_size_ -= 1;//decrement cur_size_
 	
 	if(cur_size_ == 0)
-	{
+	{//if this emptied the array, mark it as such
 		is_empty_ = true;
 	}
 	
@@ -120,7 +124,9 @@ T Queue <T>::dequeue (void)
 //
 template <typename T>
 void Queue <T>::clear (void)
-{
+{//reset inside elements to default
+ //there shouldn't be a way to get to the old data,
+ //so there isn't really a need to delete said data
 	cur_size_ = 0;
 	is_empty_ = true;
 }
